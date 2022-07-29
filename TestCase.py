4755cc4eb4ecbe8683949b9e5f555e8c61daf86f
Jurandi <jur.and.jr42@gmail.com>
2022-07-29 08:28:32 -0300

    Criação da superclasse TestCase; ascensão do método run à superclasse

---
+++ TestCase.py
@@ -0,0 +1,8 @@
+class TestCase:
+
+    def __init__(self, name):
+        self.name = name
+
+    def run(self):
+        method = getattr(self, self.name)
+        method()
--- WasRun.py
+++ WasRun.py
@@ -1,10 +1,12 @@
-class WasRun:
+from TestCase import TestCase
+
+
+class WasRun(TestCase):
 
     def __init__(self, name):
         self.wasRun = None
+        TestCase.__init__(self, name)
 
     def testMethod(self):
         self.wasRun = 1
 
-    def run(self):
-        self.testMethod()
