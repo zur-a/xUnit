e4b917778fc4b34ee426f874a8786629c1e34f8b
Jurandi <jur.and.jr42@gmail.com>
2022-07-29 07:58:25 -0300

    Definição da classe WasRun

---
+++ WasRun.py
@@ -0,0 +1,4 @@
+class WasRun:
+
+    def __init__(self, name):
+        self.wasRun = None
---
+++ main.py
@@ -0,0 +1,7 @@
+from WasRun import WasRun
+
+test = WasRun("testMethod")
+print(test.wasRun)
+
+test.testMethod()
+print(test.wasRun)
