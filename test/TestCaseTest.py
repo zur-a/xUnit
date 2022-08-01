2a8b47b7a8088ed1dc176d90a0852d26ae72c27c
Jurandi <jur.and.jr42@gmail.com>
2022-08-01 09:03:24 -0300

    Criação do teste resultTest

--- test/TestCaseTest.py
+++ test/TestCaseTest.py
@@ -10,10 +10,14 @@ class TestCaseTest(TestCase):
     def templateMethodTest(self):
         test = WasRun("testMethod")
         test.run()
-        assert ("setUp testMethod tearDown" == test.log)
+        assert ("setUp testMethod tearDown " == test.log)
 
-
+    def resultTest(self):
+        test = WasRun("testMethod")
+        result = test.run()
+        assert("1 run, 0 failed" == result.summary())
 
 
 TestCaseTest("templateMethodTest").run()
+TestCaseTest("resultTest").run()
 
